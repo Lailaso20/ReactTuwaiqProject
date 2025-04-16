c14c4d5b14120b2b953d251108d8c6ce7c579a11
Laila Almutairi <Lulo.1410@gmail.com>
2025-04-16 19:08:39 +0300

    add users

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 
 function User() {
+  const [user, setUsers] = useState ([]);
+  const fetchUsers = async ()=>{
+    const res = await axios.get('https://jsonplaceholder.typicode.com/users');
+    console.log(res);
+const userData =res.data;
+setUsers(userData);
+  };
   return <div>User</div>;
 }
 
